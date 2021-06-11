f8562503ead5f2c919ebd5f3fb29167ccf89854d
bkatovic <borna.katovic@fer.hr>
2021-06-11 09:09:37 +0200

    turned off debug for production

--- stela/settings.py
+++ stela/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','student-election-atlas.herokuapp.com']
 
